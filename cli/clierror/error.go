9f5946f592010b39d7ba87d9264c79a7c9ea3ed3
Chris <github.account@chrigel.net>
2021-07-27 22:57:42 +0200

    Follow the Go convention in naming error type

--- cli/clierror/error.go
+++ cli/clierror/error.go
@@ -9,11 +9,11 @@ import (
 )
 
 var (
-	// UsageError is an error that is caused by an incorrect usage by the user.
-	UsageError = errors.New("usage error")
-	// ErrorHandler will print the CLI help and exit with code 2 if the given error is a UsageError.
+	// ErrUsage is an error that is caused by an incorrect usage by the user.
+	ErrUsage = errors.New("usage error")
+	// ErrorHandler will print the CLI help and exit with code 2 if the given error is a ErrUsage.
 	ErrorHandler cli.ExitErrHandlerFunc = func(context *cli.Context, err error) {
-		if errors.Is(err, UsageError) {
+		if errors.Is(err, ErrUsage) {
 			printer.DefaultPrinter.ErrorF("%v\n", err.Error())
 			cli.ShowCommandHelpAndExit(context, context.Command.Name, 2)
 			return
@@ -21,22 +21,22 @@ var (
 	}
 )
 
-// AsUsageError returns the given error wrapped in a UsageError.
+// AsUsageError returns the given error wrapped in a ErrUsage.
 func AsUsageError(err error) error {
-	return fmt.Errorf("%w: %v", UsageError, err)
+	return fmt.Errorf("%w: %v", ErrUsage, err)
 }
 
-// AsUsageErrorf returns a UsageError with the given format
+// AsUsageErrorf returns a ErrUsage with the given format
 func AsUsageErrorf(format string, a ...interface{}) error {
 	return AsUsageError(fmt.Errorf(format, a...))
 }
 
-// AsFlagUsageError returns a flag UsageError with the given error.
+// AsFlagUsageError returns a flag ErrUsage with the given error.
 func AsFlagUsageError(flagName string, err error) error {
 	return AsUsageError(fmt.Errorf("invalid flag --%s: %v", flagName, err))
 }
 
-// AsFlagUsageErrorf returns a flag UsageError with the given message.
+// AsFlagUsageErrorf returns a flag ErrUsage with the given message.
 func AsFlagUsageErrorf(flagName, format string, a ...interface{}) error {
 	return AsUsageErrorf("invalid flag --%s: %s", flagName, fmt.Sprintf(format, a...))
 }
