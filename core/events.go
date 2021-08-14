625ec9a559b2be584bbeeebb114cc015fa79b0db
Chris <github.account@chrigel.net>
2021-08-14 12:37:11 +0200

    Add EventProperties
    This allows passing properties and other data to event handlers that require them.

--- core/events.go
+++ core/events.go
@@ -4,10 +4,30 @@ import (
 	"fmt"
 )
 
+type EventProperties map[string]interface{}
+
+// EventProperty provides accessor methods for retrieving values from EventProperties.
+type EventProperty interface {
+	// BoolProperty returns a boolean value identified by key.
+	// It returns defaultValue if
+	//  * EventSource.Properties are nil
+	//  * the value of key doesn't exist
+	//  * the value of key cannot be converted to boolean from a string or int (0 or 1 only)
+	BoolProperty(key string, defaultValue bool) bool
+	// StringProperty returns a string value identified by key.
+	// It returns defaultValue if
+	//  * EventSource.Properties are nil
+	//  * the value of key doesn't exist
+	StringProperty(key string, defaultValue string) string
+}
+
 // EventSource represents a uniform input value for event processing.
 type EventSource struct {
 	// Url is a property that uniquely identifies the Git repository that was involved in the event handling.
 	Url *GitURL
+	// Properties is a map containing values that may be relevant for the event handler.
+	// Event receiver must handle nil values.
+	Properties EventProperties
 }
 
 // EventResult represents a uniform return value for events.
@@ -16,12 +36,15 @@ type EventResult struct {
 	Url *GitURL
 	// Error can optionally contain any error that occurred during event handling.
 	Error error
+	// Properties is a map containing values that may be relevant for the event handler.
+	// Event receiver must handle nil values.
+	Properties EventProperties
 }
 
 // EventHandler is a service that can handle certain configured event types.
 type EventHandler interface {
 	// Handle processes the given EventSource synchronously.
-	// Any errors are to be defined in the EventResult::Error property.
+	// Any errors are to be defined in the EventResult.Error property.
 	Handle(source EventSource) EventResult
 }
 
