0b6156b94842d67359d77ebac2c871f9efc7b705
Chris <github.account@chrigel.net>
2022-04-24 18:38:41 +0200

    Document usage in help text

--- application/test/command.go
+++ application/test/command.go
@@ -42,8 +42,25 @@ func (c *Command) GetCliCommand() *cli.Command {
 
 func (c *Command) createCliCommand() *cli.Command {
 	return &cli.Command{
-		Name:   "test",
-		Usage:  "Test the rendered template against test cases",
+		Name:  "test",
+		Usage: "Test the rendered template against test cases",
+		Description: `Test cases are defined as local, simulated repositories in 'tests' directory, where each subdirectory itself is a separate test case.
+The expected file structure should resemble this format: 
+
+tests
+└── case-1 
+    ├── <file>
+    └── .sync.yml
+
+'case-1' is the test case name.
+<file> represents any files that are to be rendered (for example README.md, Makefile etc.) with their contents being the desired output.
+'.sync.yml' is the sync config for this simulated repository and it works exactly as the .sync.yml syntax in real repositories.
+
+When running this subcommand, these test cases are picked up and its template output rendered in a new directory '.tests'.
+A 'git diff' will be computed and if it's non-empty, the test case is considered failed.
+
+This command can be used to verify that the template is correct before rolling it out to production repositories.
+`,
 		Action: c.runCommand,
 		Before: c.validateTestCommand,
 		Flags: flags.CombineWithGlobalFlags(
