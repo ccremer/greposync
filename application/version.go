5f7f7130e5e59651661ff37027710b06488c939b
Chris <github.account@chrigel.net>
2022-05-07 18:36:28 +0200

    Generate help text for each command in the documentation

--- application/version.go
+++ application/version.go
@@ -12,7 +12,15 @@ type VersionInfo struct {
 }
 
 func (vi VersionInfo) String() string {
-	dateLayout := "2006-01-02"
-	t, _ := time.Parse(dateLayout, vi.Date)
-	return fmt.Sprintf("%s, commit %s, date %s", vi.Version, vi.Commit[0:7], t.Format(dateLayout))
+	date := ""
+	if vi.Date != "" {
+		dateLayout := "2006-01-02"
+		t, _ := time.Parse(dateLayout, vi.Date)
+		date = t.Format(dateLayout)
+	}
+	commit := vi.Commit
+	if len(commit) > 7 {
+		commit = vi.Commit[:7]
+	}
+	return fmt.Sprintf("%s, commit %s, date %s", vi.Version, commit, date)
 }
